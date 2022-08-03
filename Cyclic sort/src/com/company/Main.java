eb711528c7e2c654b27f656b194858574ffe4a55
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2022-08-03 19:51:33 +0530

    new files

---
+++ Cyclic sort/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+
+// T.C = O(n)
+// S.C = O(1)
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}

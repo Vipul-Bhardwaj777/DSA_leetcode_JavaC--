c8ec99e383cf6eb692b16ee0d856fec325ede03e
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2022-08-03 19:30:13 +0530

    Bubble sort

--- Bubble sort/src/com/company/Main.java
+++ Bubble sort/src/com/company/Main.java
@@ -11,7 +11,8 @@ public class Main {
         int temp = 0;
         boolean swap;
         for(int i=0; i < n; i++){
-            swap = false;
+            
+		swap = false;
             for(int j=1; j < (n-i); j++){
                 if(arr[j-1] > arr[j]){
                     //swap elements

867f57e26bb1ec0b89fdd501fbf73530c8611381
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-04-17 18:24:44 +0530

    Time: 2 ms (95.87%), Space: 42.9 MB (40.82%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,33 +1,40 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        
-        // Two pointer method
+          // Two pointer method
         
         int n = nums1.length, m = nums2.length;
         
+        // Create a list
         List<Integer> list = new ArrayList<>();
         
+        // Then sort both the arrays
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         
         int i=0, j=0;
         
+        // Traverse the arrays  and compare the elements
         while(i<n && j<m){
             
+            // If the elements are same we get one intersection and add to list 
             if(nums1[i] == nums2[j]){
-                list.add(nums1[i]);
+                
+                    list.add(nums1[i]);
                 i++;
                 j++;
+                
             }
             
+            // If nums1[i] < nums2[j] this means that there can't be an intersection if we move j as array is sorted
             else if(nums1[i] < nums2[j]) i++;
             
+            // If  nums1[i] > nums2[j] this means that there can't be an intersection if we move i as array is sorted
             else{ j++; }
             
         }
         
         
-        
+        // Return the list after converting it into an array
         int[] ans = new int[list.size()];
         
         for(int k = 0; k < list.size(); k++){

d0b48bcfaa2f768174a614ad4e79e48f327d565d
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-04-17 18:20:43 +0530

    Time: 2 ms (79.73%), Space: 40.2 MB (28.85%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,21 +1,22 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long s=1,e=n;
-        long ans=-1;
+        
+        long s=1,e=n;  // int will overflow so long is used
+        
         while(s<=e)
             {
-            long m=s+(e-s)/2;
+            long m=s+(e-s)/2;   
             
-           
-             if((m*(m+1))/2<=
-                n)
+            if((m*(m+1))/2 == n) return (int) m;   // m*(m+1)/2 gives the sum of all nos upto m and if sum of all nos upto midpoint = n this means n coins will perfectly complete the m no of stairs   
+                
+             if((m*(m+1))/2 < n)
                  {
-                ans= m;
+                
                  s=m+1;
                  }
             
             else{ e=m-1;}
             }
-        return (int)ans;
+        return (int) e; // loop will break if perfect ans is not found so ans will be at end  
     }
 }

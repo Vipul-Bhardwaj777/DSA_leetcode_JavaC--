b1cbd9d9fae06f2f3ba27596bde7fa88e480aef6
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-04-18 19:23:12 +0530

    Time: 1 ms (62.50%), Space: 40.4 MB (49.02%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -2,23 +2,32 @@ class Solution {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
         int len = nums.length;
-        int left = 0;
-        int right = len - 1;
-        // binary search to find the first position that
-        // nums[i] >= len - i and nums[i - 1] < len - i
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= len - mid) {
-                // treat index 0 to avoid IndexOutOfBoundError
-                if (mid == 0 || nums[mid - 1] < len - mid) { // correct index
-                    return len - mid;
-                } else { // not the first position, shrink right bound
-                    right = mid - 1;
-                }
-            } else { // otherwisem, shrink the left bound to increase nums[mid]
-                left = mid + 1;
+        // enumerate all possible number i
+        for (int x = 0; x <= nums[len - 1]; x++) {
+            // find the first index that nums[idx] >= i
+            int idx = findFirstGreaterOrEqual(x, nums);
+            if (len - idx == x) {
+                return x;
             }
         }
         return -1;
+    }
+
+    private int findFirstGreaterOrEqual(int target, int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        if (nums[left] >= target) {
+            return left;
+        }
+        return right; // whether right >= target or right > target
     }
 }

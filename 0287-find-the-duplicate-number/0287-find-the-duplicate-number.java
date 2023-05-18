e05fbba47d87038a6e52a170870215a889824ea1
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-05-18 13:09:52 +0530

    Time: 6 ms (52.25%), Space: 59.8 MB (59.60%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,43 +1,32 @@
 /*
   Range is 1-n so again Cyclic sort and given to solve in linear O(n) time complexity
-  
-  Just one additional case when we find that the nums[i] is at correct index, it may be the case that we are on a duplicate number, so we check if its duplicate then its i+1 != nums[i]
-  And if its not duplicate, simply i++
+  After applying cyclic sort the repeating number always comes at the last index
   
 */
 class Solution {
     public int findDuplicate(int[] nums) {
-        
+
         int i = 0, n = nums.length;
-        
         while(i < n){
             int correctidx = nums[i]-1;
-            
+
             if(nums[correctidx] != nums[i] ){
                 swap(nums, i, correctidx);
             }
-            
-            else if(nums[correctidx] == nums[i] && i+1 != nums[i]){  // If we are at a duplicate no 
-                
-                return nums[i];
-            }
-            
-            else{  // If we are not at a duplicate number
 
-            i++;
-            }
-            
-            
+            else{ i++; }
+
+
         }
-        
-        
-      return -1;  
+
+        return nums[n-1];
+
     }
-    
+
     void swap(int[] arr, int x, int y){
-        
+
         int temp = arr[x];
         arr[x] = arr[y];
         arr[y] = temp;
     }
-}
+} 

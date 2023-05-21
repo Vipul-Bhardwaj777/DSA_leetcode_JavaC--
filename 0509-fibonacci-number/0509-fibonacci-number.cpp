32aaa428dbcd7c6a5af1f9d6bd4f75e5b320f776
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-05-21 23:22:57 +0530

    Time: 13 ms (40.85%), Space: 5.9 MB (94.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,7 +4,9 @@ public:
         
         if(n==0)return 0;
         if(n==1)return 1;
-        return( fib(n-1)+fib(n-2));
         
+        else {
+            return( fib(n-1)+fib(n-2));
+        }
     }
 };

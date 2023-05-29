208fb6e860a251c948569ff986da05a2eb744a28
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-05-29 20:54:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.53%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -5,7 +5,8 @@ public:
         if(n==1)return true;
         if(n==0)return false;
       
-        return(n>0&&!(n & n-1));
+        return( n > 0 && (n & n-1) == 0 ); 
+        // power of 2 consists of 1 setbit and all 00's eg 1000 = 111 + 1 where 111 = n-1 and if we and it we'll get 0 if it's a pow of 2
         
         
         

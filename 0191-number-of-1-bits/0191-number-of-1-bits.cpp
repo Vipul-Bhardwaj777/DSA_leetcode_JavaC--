cc77aef7091a2b41907fb67108ba64abc841398d
Vipul Bhardwaj <98729146+Vipul-Bhardwaj777@users.noreply.github.com>
2023-05-29 20:53:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.34%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        
         int c=0;
-        while(n!=0)
+        
+        while(n > 0)                   // Running with O(logn) T.C as running no of bits times
             {
-            n=n&n-1;
-            c++;
+            
+             if(n & 1 == 1) c++;       // find LSP and c++ if its a setbit
+            
+              n >>= 1;                 // move to next bit
+           
             }
         return c;
         
